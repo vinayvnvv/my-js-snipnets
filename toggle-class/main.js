16a3805acc4279403d6479455679b4b22e49ec0e
Vinay <Vinay.bv@accionlabs.com>
2017-05-05 18:41:23 +0530

    changes

--- toggle-class/main.js
+++ toggle-class/main.js
@@ -36,8 +36,66 @@
 
 	}	
 
+
+
+	function initAddClass() {
+	    console.log("init add class")
+		var tClass = document.querySelectorAll('[add-class]');
+		console.log(tClass)
+		for(var i=0;i<tClass.length;i++) {
+			tClass[i].onclick = aClassPerform;
+		}
+
+
+		function aClassPerform(ref) {
+			var src = ref.target;
+			obj = JSON.parse(src.attributes.getNamedItem('add-class').value);
+			var tClassFor = document.querySelectorAll('[add-class-id]');
+			for(var i=0;i<tClassFor.length;i++) {
+				var id = tClassFor[i].attributes.getNamedItem('add-class-id').value;
+				if(id == obj.for) {
+	                   tClassFor[i].classList.add(obj.class)
+					
+				}
+			}
+		}
+
+	}
+
+
+
+	function initRemoveClass() {
+	    console.log("init remove class")
+		var tClass = document.querySelectorAll('[remove-class]');
+		console.log(tClass)
+		for(var i=0;i<tClass.length;i++) {
+			tClass[i].onclick = rClassPerform;
+		}
+
+
+		function rClassPerform(ref) {
+			var src = ref.target;
+			obj = JSON.parse(src.attributes.getNamedItem('remove-class').value);
+			var tClassFor = document.querySelectorAll('[remove-class-id]');
+			for(var i=0;i<tClassFor.length;i++) {
+				var id = tClassFor[i].attributes.getNamedItem('remove-class-id').value;
+				if(id == obj.for) {
+	                   tClassFor[i].classList.remove(obj.class)
+					
+				}
+			}
+		}
+
+	}	
+
+
+
+
+
     //init after DOM load
 	document.addEventListener('DOMContentLoaded', initToggleClass , false);
+	document.addEventListener('DOMContentLoaded', initAddClass , false);
+	document.addEventListener('DOMContentLoaded', initRemoveClass , false);
 
 }) ();
 
